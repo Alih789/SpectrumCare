7a620513c295cf83aa5e1506932b4ef92decf7f6
George Ortega <gotega12@gmail.com>
2023-02-13 23:43:06 -0800

    added ContactPage from navigation branch

---
+++ pages/ContactPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+function HomePage(): JSX.Element {
+
+    return (
+      <View>
+        <Text style = {{alignSelf:"center"}}>Contact Page Placeholder</Text>
+      </View>
+    );
+
+}
+
+export default HomePage;
