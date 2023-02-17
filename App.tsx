3f61a575c6c955b23006ad161ac428ee2aeaf1e5
Noelle Dea <noelledea@gmail.com>
2023-02-17 15:37:53 -0800

    add r->l slide animation and clean App,tsx

--- App.tsx
+++ App.tsx
@@ -1,59 +1,65 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-
 import 'react-native-gesture-handler';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import PrepPage from './pages/PrepPage';
 import WayfindingPage from './pages/WayfindingPage';
-
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import WFRoutePage from './pages/WFRoutePage';
-import { createStackNavigator } from '@react-navigation/stack';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 function Wayfinding() {
   return (
-    <Stack.Navigator>
-    <Stack.Screen
-      name="WayfindingHome"
-      component={WayfindingPage}
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen name="Route" component={WFRoutePage} />
-  </Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+      }}>
+      <Stack.Screen
+        name="WayfindingHome"
+        component={WayfindingPage}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Route" component={WFRoutePage} />
+    </Stack.Navigator>
   )
 }
 
 function App(): JSX.Element {
 
-    return (
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size }) => {
-              let iconName:string = "";
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName: string = "";
 
-              if (route.name === 'Home') {
-                iconName = 'home-outline';
-              } else if (route.name === 'Contact') {
-                iconName =  'people-circle-outline'
-              } else if (route.name === 'Prep') {
-                iconName =  'list-outline'
-              } else if (route.name === 'Wayfinding') {
-                iconName =  'map-outline'
-              }
+            if (route.name === 'Home') {
+              iconName = 'home-outline';
+            } else if (route.name === 'Contact') {
+              iconName = 'people-circle-outline'
+            } else if (route.name === 'Prep') {
+              iconName = 'list-outline'
+            } else if (route.name === 'Wayfinding') {
+              iconName = 'map-outline'
+            }
 
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: '#00b2e3',
-            tabBarInactiveTintColor: '#999999',
-            headerShown: false,
-          })}>
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#00b2e3',
+          tabBarInactiveTintColor: '#999999',
+          headerShown: false,
+
+        })}>
         <Tab.Screen
           name="Home"
           component={HomePage}
@@ -71,9 +77,9 @@ function App(): JSX.Element {
           component={Wayfinding}
         />
       </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
+    </NavigationContainer>
+  );
+}
 
 
 const styles = StyleSheet.create({
