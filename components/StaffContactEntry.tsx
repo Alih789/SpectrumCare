c3d770c79af5fd1b77dc548beea87d267b3f0ca0
Sergio Santoyo <santoyosergio14@gmail.com>
2023-03-04 19:39:56 -0800

    Initial addition of favButton + CSS fix for cards

--- components/StaffContactEntry.tsx
+++ components/StaffContactEntry.tsx
@@ -1,32 +1,70 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import {Image, StyleSheet, Text, View, Button, Pressable} from 'react-native'
 import React from 'react'
 
+type itemProps ={name: string, imagePath: any, jobTitle: string, department: string}
 
-function StaffContactEntry(): JSX.Element {
+function StaffContactEntry({name, imagePath, jobTitle, department}: itemProps): JSX.Element {
+
+  const handleFavPress = () => {
+    console.log("Button pressed!")
+  }
+  
   return (
-    <View style={styles.cell}>
-          <Image source={require("../assets/images/staffImages/Victoria_R_Immunolgy.jpeg")} style={styles.placeholderImg} />
-          <Text>Victoria R. Dimitriades, M.D.</Text>
+    <View style={styles.container}>
+      <Image source={imagePath} style={styles.image}/>
+      <View style={styles.detailsContainer}>
+        <Pressable style={styles.favButton} onPress={handleFavPress}>
+          <Text>Button</Text>
+        </Pressable>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+        <Text style={styles.department}>{department}</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    cell: {
-        backgroundColor: 'white',
-        flexDirection: 'row',
-        borderStyle: 'solid',
-        borderWidth: 2,
-        height: 200,
-        padding: 20,
-        borderRadius: 15,
-      },
-      Text:{
-        width: "100%",
-      },
-      placeholderImg: {
-        width: "40%", 
-        height: 150,
-      },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    height: 200,
+    backgroundColor: 'white',
+    borderRadius: 15,
+    borderWidth: 2,
+  },
+  image: {
+    width: 100,
+    height: 150,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  detailsContainer: {
+    flexDirection: 'column',
+    flexShrink: 1,
+    position: 'relative',
+  },
+  favButton:{
+    position: "absolute",
+    top: -25,
+    right: 0,
+  },
+  name: {
+    flexWrap: "wrap",
+    fontWeight: 'bold',
+    fontSize: 14,
+    paddingBottom: 3,
+  },
+  jobTitle: {
+    flexWrap: "wrap",
+    fontWeight: "600",
+    fontSize: 10,
+  },
+  department: {
+    flex: 0.6,
+    flexWrap: "wrap",
+    fontSize: 9,
+  },
 })
 export default StaffContactEntry
--- pages/ContactPage.tsx
+++ pages/ContactPage.tsx
@@ -1,39 +1,112 @@
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { StyleSheet, Text, SafeAreaView, useWindowDimensions, FlatList } from 'react-native';
 import StaffContactEntry from '../components/StaffContactEntry';
-
-import NotesButton from '../components/NotesButton';
+// import NotesButton from '../components/NotesButton';
 import SearchBar from "react-native-dynamic-search-bar";
+import Fuse from 'fuse.js';
 
+// const staffInfo = require('../assets/staffDirectory/staffContactData.json');
 
-function HomePage(): JSX.Element {
+function ContactPage(): JSX.Element {
 
-  const images =[
-    'https://physicians.ucdavis.edu/Custom/Photos/22079.jpg',
+  const staffInfo = [
+    {
+      "id": "01-Pediatric-Allergy-Immunology-and-Rheumatology",
+      "name": "Victoria R. Dimitriades, M.D.",
+      "imagePath": require("../assets/images/staffImages/Victoria_R_Immunolgy.jpeg"),
+      "department": "Chief, Division of Pediatric Allergy, Immunology and Rheumatology",
+      "jobTitle": "Clinical Professor, Department of Pediatrics"
+    },
+    {
+      "id": "02-Pediatric-Allergy-Immunology-and-Rheumatology",
+      "name": "Sheryl J. Boon, M.D., M.S.P.H.",
+      "imagePath": require("../assets/images/staffImages/Sheryl_Rheumatology.jpeg"),
+      "department": "Division of Pediatric Allergy, Immunology and Rheumatology",
+      "jobTitle": "Clinical Professor"
+    },
+    {
+      "id": "03-Pediatric-Allergy-Immunology-and-Rheumatology",
+      "name": "Angel Alberto Herrera Guerra, M.D.",
+      "imagePath": require("../assets/images/staffImages/Angel_Rheumatology.jpeg"),
+      "department": "Division of Pediatric Allergy, Immunology and Rheumatology",
+      "jobTitle": "Associate Professor"
+    },
+    {
+      "id": "04-Pediatric-Allergy-Immunology-and-Rheumatology",
+      "name": "Anh Phuong Nguyen, M.D., M.P.H.",
+      "imagePath": require("../assets/images/staffImages/Anh_Rheumatology.jpeg"),
+      "department": "Division of Pediatric Allergy, Immunology and Rheumatology",
+      "jobTitle": "Assistant Clinical Professor"
+    },
+    {
+      "id": "05-CAARE-Diagnostic-and-Treatment-Center",
+      "name": "Victoria R. Dimitriades, M.D.",
+      "imagePath": require("../assets/images/staffImages/Victoria_R_Immunolgy.jpeg"),
+      "department": "Chief, Division of Pediatric Allergy, Immunology and Rheumatology",
+      "jobTitle": "Clinical Professor, Department of Pediatrics"
+    },
   ];
 
-  const Names = [
-  ];
+
+
+  //used to store full data source
+  const [fullData, setFullData] = useState(staffInfo);
+  //used to store filtered data based on the search
+  const [searchData, setSearchData] = useState(staffInfo);
+  //stores current searched term 
+  const [searchTerm, setSearchTerm] = useState('');
+  //adjust the background to appear when searching for specific names
+  const { height } = useWindowDimensions();
+
+  const options = {
+    keys: ["name"],
+    //search score for how close the match is to the actual string
+    includeScore: true,
+    threshold: 0.3,
+    //min number of char required to in search to make sure matcb is valid
+    minMatchCharLength: 3,
+    //max length of the search
+    maxPatternLength: 32,
+  };
+
+  const fuse = new Fuse(fullData, options);
+
+  const handleSearch = (text: string) => {
+    if (text.length == 0) {
+      setSearchData(fullData);
+    } else {
+      const results = fuse.search(text);
+      const filteredData = results.map((result) => result.item);
+      setSearchData(filteredData);
+    }
+    setSearchTerm(text);
+  };
 
   return (
     <SafeAreaView style={styles.background}>
-      <ScrollView>
-        
-        <Text style={styles.headerText}> Staff Contact List </Text>
-        <SearchBar
-          placeholder="Search here"
-          onChangeText={(text) => console.log(text)}
-        />
-        <ScrollView style={styles.scrollView}>
-          <StaffContactEntry />
-          <StaffContactEntry />
-          <StaffContactEntry />
-          <StaffContactEntry />
-          <StaffContactEntry />
-        </ScrollView>
-        <NotesButton/>
-      </ScrollView>
+      <Text style={styles.headerText}> Staff Contact List </Text>
+      <SearchBar
+        placeholder="Search"
+        onChangeText={handleSearch}
+        value={searchTerm}
+        onClearPress={() => {
+          setSearchData(fullData);
+          setSearchTerm('')
+        }}
+        style={styles.searchBar}
+      />
+      <FlatList
+        data={searchData}
+        renderItem={({ item }) =>
+          <StaffContactEntry
+            name={item.name}
+            imagePath={item.imagePath}
+            jobTitle={item.jobTitle}
+            department={item.department}
+          />}
+        keyExtractor={item => item.id}
+        style={[styles.list, { height: height - 150 }]}
+      />
     </SafeAreaView>
   );
 }
@@ -42,9 +115,8 @@ const styles = StyleSheet.create({
   background: {
     backgroundColor: "#003A5D",
   },
-  scrollView: {
-    marginHorizontal: 25,
-    paddingTop: 10,
+  list: {
+    marginHorizontal: 20,
   },
   headerText: {
     paddingTop: 30,
@@ -54,6 +126,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 36,
   },
+  searchBar: {
+    borderRadius: 10,
+    padding: 5,
+    marginBottom: 10,
+  },
 });
 
-export default HomePage;
+export default ContactPage;
