6e1175518463d958113f7dbf1557b0c1b77eb37e
Noelle Dea <noelledea@gmail.com>
2023-03-07 11:19:03 -0800

    fix direction wayfinding hospital

--- pages/WFRoutePage.tsx
+++ pages/WFRoutePage.tsx
@@ -168,7 +168,7 @@ function WFRoutePage({ navigation, route }: Props): JSX.Element {
       "This is the main entrance, walk through these doors.",
       "Second set of doors.",
       "Keep walking, head towards this waterfall mosaic.",
-      "From the waterfall, walk to the right.",
+      "From the waterfall, walk to the left.",
       "You will see this view of the lobby. Walk to the right, where the arrow points \"Other Hospital Destinations\".",
       "Walk down the corridor to the elevator bay.",
       "At the end of the elevator bay, turn right here.",
@@ -220,7 +220,7 @@ function WFRoutePage({ navigation, route }: Props): JSX.Element {
       "This is the main entrance, walk through these doors.",
       "Second set of doors.",
       "Keep walking, head towards this waterfall mosaic.",
-      "From the waterfall, walk to the right.",
+      "From the waterfall, walk to the left.",
       "You will see this view of the lobby. Walk to the right, where the arrow points \"Other Hospital Destinations\".",
       "Walk down the corridor to the elevator bay. Get on any elevator and go to the 2nd floor.",
       "When you exit the elevator, you will be in this 2nd floor elevator bay.",
@@ -334,7 +334,7 @@ const styles = StyleSheet.create({
   },
   carousel: {
     // marginTop: 50
-    backgroundColor: 'red'
+    // backgroundColor: 'red'
   },
   headerText: {
     fontSize: 22,
