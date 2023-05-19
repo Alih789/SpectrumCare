ecabd0f2d0463dbc26f77d48b54b32403080cbc1
Sergio Santoyo <62675343+ssantoyo@users.noreply.github.com>
2023-05-19 14:07:13 -0700

    All Staff Search is now working

--- pages/ContactPage.tsx
+++ pages/ContactPage.tsx
@@ -20,30 +20,33 @@ type itemProps = {
   hyperlink: string
 }
 
-
 function ContactPage(): JSX.Element {
   
   //used to store All Staff data source
   const [fullData, setFullData] =  useState<itemProps[]>([]);
-  const [searchFullData, setSearchFullData] = useState<itemProps[]>([]);
+  
+  const adjFullData = [...fullData];
+  const [searchFullData, setSearchFullData] = useState(adjFullData);
+    
   const [loading, setLoading] = useState(true);
+  const [loadMoreInProgress, setLoadMoreInProgress] = useState(false);
   const [lastVisible, setLastVisible] = useState<FirebaseFirestoreTypes.DocumentSnapshot>();
-
+  
   //used to store Favorite Staff data source
   const [favData, setFavData] = useState<itemProps[]>([]);
   const [favColors, setFavColors] = useState({});
-
+  
   //used to store Fav filtered data based on the search
   const [searchFavData, setSearchFavData] =  useState<itemProps[]>([]); 
-
+  
   //stores current searched term
   const [searchTerm, setSearchTerm] = useState('');
   //adjust the background to appear when searching for specific names
   const { height } = useWindowDimensions();
-
+  
   //determines which tab you are on
   const [activeTab, setActiveTab] = useState('AllStaff')
-
+  
   async function getImageUrl(imagePath: string){
     const imageRef = storage().ref(imagePath);
     try {
@@ -57,31 +60,39 @@ function ContactPage(): JSX.Element {
       return "";
     }
   }
-
+  
   async function loadMore(){
+
+    if(loadMoreInProgress){
+      //request has already been made, do nothing
+      return;
+    }
     //fetches the next set of data from the database 
     //lastVisible carries the last entry from the loaded batch recorded by the fetch
     try {
+      setLoadMoreInProgress(true);
       //Creating a ref for the collection we are trying to query
       const collectionRef = firestore().collection('staff-list');
       
       //query the next set of results
       let query = collectionRef.orderBy('name');
-
+      
       //if we havent reached the end of the list 
       if(lastVisible){
         query = query.startAfter(lastVisible);
+      }else{
+        return;
       }
-       
+      
       const snapshot = await query.get();
-
+      
       const newData = await Promise.all(snapshot.docs.map(async (doc) => {
         const data = doc.data();
-  
+        
         //imagePath is then turned into a url with google token
         const downloadUrl = await getImageUrl(data.imagePath);
         //creating new objects with updated information
-
+        
         return {
           id: doc.id,
           name: data.name,
@@ -94,23 +105,25 @@ function ContactPage(): JSX.Element {
         };
       }));
       //updates the fullData array by appending the new Data to it
-      setFullData([...fullData, ...newData]);
+      setFullData((prevData) => [...prevData, ...newData]);
       //updates the last document fetched from the db and starts the next batch from that point
       setLastVisible(snapshot.docs[snapshot.docs.length-1])
     } catch(error) {
       console.log('Error fetching next set of results: ', error);
+    }finally{
+      setLoadMoreInProgress(false);
     }
   }
-
+  
   //Handles query to database
   useEffect( ()=> {
     async function getStaffList(){
       const staffSnapshot = (await firestore().collection('staff-list').orderBy('name').limit(5).get());
       const newData = await Promise.all(staffSnapshot.docs.map(async (doc) => {
         const data = doc.data();
-
+        
         const downloadUrl =  await getImageUrl(data.imagePath);
-
+        
         return {
           id: doc.id,
           name: data.name,
@@ -122,111 +135,75 @@ function ContactPage(): JSX.Element {
           hyperlink: data.hyperlink,
         };
       }));
-
+      
       setFullData(newData);
       setLastVisible(staffSnapshot.docs[staffSnapshot.docs.length-1]);
       //setloading lets us know that the data has finished loading on screen
       setLoading(false);
     }
     getStaffList();
-  }, [])
+  }, []);
 
-  //handles updating the display based on what we get from the database
-  useEffect( ()=>{
-    setSearchFullData(fullData)
-  }, [fullData])
-
-
-
-    //handles updating the display based on what we get from the favData
-  useEffect( ()=>{
-    setSearchFavData(favData)
-  }, [favData])
-  
-  const handleFavPress = (staffId: string) => {
-    const staff:any = fullData.find((s) => s.id === staffId);
-
-    //If the staff isnt in the list add them
-    if(!favData.includes(staff)) {
-      setFavData(favData.concat(staff));
-      setFavColors({...favColors, [staffId]: "red"});
-    //Staff is in the list, remove them
-    } else {
-      setFavData(favData.filter((s) => s.id !== staffId));
-      setFavColors({...favColors, [staffId]: "gray"});
-    }
-  }
-
-  const options = {
-    keys: ["department"],
-    //search score for how close the match is to the actual string
-    includeScore: true,
-    threshold: 0.3,
-    //min number of char required to in search to make sure matcb is valid
-    minMatchCharLength: 1,
-    //max length of the search
-    maxPatternLength: 32,
-  };
-
-  const fuseFull = new Fuse(fullData, options);
-  const fuseFav = new Fuse(favData, options);
 
   const handleSearch = (text: string) => {
+    
+    const options = {
+      keys: ["department", "name"],
+      //search score for how close the match is to the actual string
+      includeScore: true,
+      threshold: 0.3,
+      //min number of char required to in search to make sure matcb is valid
+      minMatchCharLength: 3,
+      //max length of the search
+      maxPatternLength: 32,
+    };
 
-    if (text.length == 0) {
-      if (activeTab == 'AllStaff'){
+    if(text.length === 0){
+      if(activeTab === "AllStaff"){
+        //No Search has occured
         setSearchFullData(fullData);
-      }
-      if (activeTab == 'Favorite'){
-        setSearchFavData(favData);
-      }
+      }//TODO : Add else if favorites return full favorites array
     } else {
-      if (activeTab == 'AllStaff'){
-        const results = fuseFull.search(text);
-        const filteredData = results.map((result) => result.item);
-        setSearchFullData(filteredData);
-      }
-      if (activeTab == 'Favorite') {
-        const resultsFav = fuseFav.search(text);
-        const filteredFavData = resultsFav.map((result) => result.item);
-        setSearchFavData(filteredFavData);
-      }
+      //TODO : Add if else for when its in AllStaff and Favorites
+      const fuseFull = new Fuse(fullData, options);
+      const results = fuseFull.search(text);
+      const filteredData = results.map((result) => result.item);
+      setSearchFullData(filteredData);
     }
     setSearchTerm(text);
   };
 
-
   const handleTabToggle = (tab: string) => {
     setActiveTab(tab);
-  };
-
+  }
+  //Displays original full data
   const renderAllStaffTab = () => {
-
     return (
     <FlatList
-        data={searchFullData}
-        renderItem={({ item }) =>
-          <StaffContactEntry
-            onPress={() => handleFavPress(item.id)}
-            name={item.name}
-            imagePath={item.imagePath}
-            jobTitle={item.jobTitle}
-            department={item.department}
-            phoneNumber={item.phoneNumber}
-            hyperlink={item.hyperlink}
-          />}
-        keyExtractor={(item) => item.id}
+        data={searchFullData.length != 0 ? searchFullData : fullData}
+        renderItem={({ item, index}) => 
+        <StaffContactEntry
+        key={item.id + '_' + index}
+        onPress={() => handleFavPress(item.id)}
+        name={item.name}
+        imagePath={item.imagePath}
+        jobTitle={item.jobTitle}
+        department={item.department}
+        phoneNumber={item.phoneNumber}
+        hyperlink={item.hyperlink}
+        />}
+        keyExtractor={(item, index) => item.id + '_' + index}
         onEndReached={loadMore}
-        onEndReachedThreshold={1.5}
+        onEndReachedThreshold={5}
         ListFooterComponent={() => (loading ? <ActivityIndicator size='large' color="#0000ff"/> : null)}
         style={[styles.list, { height: height - 150 }]}
       />
     )
   };
-
+  
+  //Displays only the favorites data
   const renderFavoriteTab = () => {
     return (
-
     <FlatList
         data={searchFavData} 
         renderItem={({ item }) =>
@@ -245,6 +222,7 @@ function ContactPage(): JSX.Element {
     )
   };
 
+  //Handles which data is displayed when tabs are clicked
   const renderActiveTab = () =>{
     if(activeTab == 'AllStaff') {
       return renderAllStaffTab();
