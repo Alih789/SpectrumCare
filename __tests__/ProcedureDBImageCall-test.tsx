994545236a89900e280104ec593102cdf122d0eb
Sergio Santoyo <62675343+ssantoyo@users.noreply.github.com>
2023-06-05 15:52:00 -0700

    Added Component render for prep with updated fetched URL

--- __tests__/ProcedureDBImageCall-test.tsx
+++ __tests__/ProcedureDBImageCall-test.tsx
@@ -1,6 +1,12 @@
+import React from 'react';
 import 'react-native';
 import storage from '@react-native-firebase/storage';
+import ProcedureList from '../components/ProcedureList';
+import renderer from 'react-test-renderer';
 
+jest.mock('@react-navigation/native', () => ({
+    useNavigation: jest.fn(),
+}));
 
 jest.mock('@react-native-firebase/storage', () =>{
     const ref = jest.fn(() => ({
@@ -52,7 +58,39 @@ describe("ContactImages", () => {
         getDownloadURLSpy.mockRestore();
 
     });
-
 });
 
+    
 
+
+
+describe("ProcedureList rendered with the fetched URL", () => {
+    
+    
+    test("renders ProcedureList", async () => {
+        const testProcedureData = require('../assets/testData/testProcedureData.json');
+    
+        const ref = storage().ref("procedureImage/Needle_Pokes_page0.jpeg");
+        const getDownloadURLSpy = jest.spyOn(ref, 'getDownloadURL').mockResolvedValue("https://firebasestorage.googleapis.com/v0/b/spectrumcare-d5380.appspot.com/o/procedureImages%2FNeedle_Pokes_page0.jpeg?alt=media&token=564fd33f-0164-4bc1-a5f0-284b0ebd41cb");
+        const url = await ref.getDownloadURL();
+    
+        //verify that the mocked call to firebase storage was made
+        expect(getDownloadURLSpy).toHaveBeenCalled;
+
+        //updated content 1
+        testProcedureData[0].pages[0].media.content = url;
+        //updated content 2
+        testProcedureData[0].pages[1].media.content = url;
+        //updated content 3
+        testProcedureData[0].pages[2].media.content = url;
+
+        const tree = renderer.create(
+            <ProcedureList
+                data={testProcedureData}
+                data-testid="container-test-id"
+            />
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
