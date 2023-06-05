c698e48002c96e13204eda988716cd9479c2866c
Sergio Santoyo <62675343+ssantoyo@users.noreply.github.com>
2023-06-05 13:44:30 -0700

    Rendered Contact Component with fetched URL

--- __tests__/ContactsDBImageCall-test.tsx
+++ __tests__/ContactsDBImageCall-test.tsx
@@ -1,8 +1,12 @@
+import React from 'react';
 import 'react-native';
 import storage from '@react-native-firebase/storage';
+import StaffContactEntry from '../components/StaffContactEntry';
+import renderer from 'react-test-renderer';
 
 
-jest.mock('@react-native-firebase/storage', () =>{
+
+jest.mock('@react-native-firebase/storage', () => {
     const ref = jest.fn(() => ({
         getDownloadURL: jest.fn(),
     }))
@@ -16,7 +20,7 @@ jest.mock('@react-native-firebase/storage', () =>{
 
 describe("ContactImages", () => {
     //both are Staff Storage URL with tokens
-    it('Get Image URL: PASS', async() => {
+    it('Get Image URL: PASS', async () => {
         const ref = storage().ref("staffImages/Armatiti_Mody.jpeg");
         const getDownloadURLSpy = jest.spyOn(ref, 'getDownloadURL').mockResolvedValue("https://firebasestorage.googleapis.com/v0/b/spectrumcare-d5380.appspot.com/o/staffImages%2FArmatiti_Mody.jpeg?alt=media&token=ced5a52a-6d7c-4aab-8953-1fa60f5941f9");
         const url = await ref.getDownloadURL();
@@ -31,10 +35,42 @@ describe("ContactImages", () => {
 
         getDownloadURLSpy.mockRestore();
 
+
+
+    });
+    //Button was pressed
+    const handlePress = () => {
+        return true;
+    }
+    //isFavorited set to true to match the color
+    test("renders StaffContactEntry with fetched URL", async () => {
+
+        const ref = storage().ref("staffImages/Armatiti_Mody.jpeg");
+        const getDownloadURLSpy = jest.spyOn(ref, 'getDownloadURL').mockResolvedValue("https://firebasestorage.googleapis.com/v0/b/spectrumcare-d5380.appspot.com/o/staffImages%2FArmatiti_Mody.jpeg?alt=media&token=ced5a52a-6d7c-4aab-8953-1fa60f5941f9");
+        const url = await ref.getDownloadURL();
+
+        //verify that the call to firebase storage was made
+        expect(getDownloadURLSpy).toHaveBeenCalled;
+
+        const tree = renderer.create(
+            <StaffContactEntry
+                name='Armaiti Mody, M.D.'
+                imagePath={url}
+                jobTitle='Associate Professor'
+                department='Pediatric Allergy, Immunology and Rheumatology'
+                phoneNumber='916-734-3112'
+                onPress={handlePress}
+                isFavorited={true}
+                hyperlink='https://health.ucdavis.edu/pediatrics/team/42849/armaiti-mody---pediatric-endocrinology-sacramento/'
+            />
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+
+        getDownloadURLSpy.mockRestore();
     });
 
 
-    it('Get Image URL: FAIL', async() => {
+    it('Get Image URL: FAIL', async () => {
         //Procedure url not Staff Contact
         const ref = storage().ref("procedureImage/Needle_Pokes_page0.jpeg");
         const getDownloadURLSpy = jest.spyOn(ref, 'getDownloadURL').mockResolvedValue("https://firebasestorage.googleapis.com/v0/b/spectrumcare-d5380.appspot.com/o/procedureImages%2FNeedle_Pokes_page0.jpeg?alt=media&token=564fd33f-0164-4bc1-a5f0-284b0ebd41cb");
@@ -49,9 +85,5 @@ describe("ContactImages", () => {
         expect(result).not.toEqual(url);
 
         getDownloadURLSpy.mockRestore();
-
-
     });
 });
-
-
